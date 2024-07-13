ee6d89941853359fb2a8f90a83f113c032d25e1f
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-07-14 00:19:45 +0500

    user and task

--- src/views/tasks/Tasks.js
+++ src/views/tasks/Tasks.js
@@ -19,21 +19,22 @@ import ReactImg from 'src/assets/images/avatars/9.jpg'
 const User = () => {
   return (
     <CCardGroup>
-      <CCard className="mb-4">
+      <div className='col-lg-4 m-1 mb-4'>
+      <CCard>
         <CCardHeader>
           <strong>Пользователь</strong>
         </CCardHeader>
         <CCardBody>
           <CForm>
             <CFormLabel>Имя</CFormLabel>
-            <CInputGroup className="mb-3">
+            <CInputGroup className="mb-1">
               <CInputGroupText>
                 <CIcon icon={cilText}></CIcon>
               </CInputGroupText>
               <CFormInput type="text" placeholder="Loading..." aria-label="" disabled readOnly />
             </CInputGroup>
             <CFormLabel>Фамилия</CFormLabel>
-            <CInputGroup className="mb-3">
+            <CInputGroup className="mb-1">
               <CInputGroupText>
                 <CIcon icon={cilText}></CIcon>
               </CInputGroupText>
@@ -46,7 +47,7 @@ const User = () => {
               />
             </CInputGroup>
             <CFormLabel>Адрес электронный почты</CFormLabel>
-            <CInputGroup className="mb-3">
+            <CInputGroup className="mb-1  ">
               <CInputGroupText>@</CInputGroupText>
               <CFormInput
                 type="text"
@@ -57,7 +58,7 @@ const User = () => {
               />
             </CInputGroup>
             <CFormLabel>Номер телефона</CFormLabel>
-            <CInputGroup className="mb-3">
+            <CInputGroup className="mb-1">
               <CInputGroupText>
                 <CIcon icon={cilPhone}></CIcon>
               </CInputGroupText>
@@ -72,15 +73,17 @@ const User = () => {
           </CForm>
         </CCardBody>
       </CCard>
-      <CCard className="mb-4">
+      </div>
+      <div className='col-lg-3 m-1 mb-4'>
+      <CCard>
         <CCardHeader>
           <strong>Фото профиля</strong>
         </CCardHeader>
         <CCardBody className="d-flex flex-column align-items-center">
           <CCardImage 
             src={ReactImg} 
-            className='m-1'
-            style={{ width: '250px', height: '250px', objectFit: 'cover', borderRadius: '50%' }} 
+            className='p-3 m-3'
+            style={{ width: '200px', height: '200px', objectFit: 'cover', borderRadius: '50%' }} 
           />
           <div className="mt-3 ">
             <CButton className="btn-primary m-1">Изменить</CButton>
@@ -88,6 +91,7 @@ const User = () => {
           </div>
         </CCardBody>
       </CCard>
+      </div>
     </CCardGroup>
   )
 }
--- src/views/user/User.js
+++ src/views/user/User.js
@@ -8,87 +8,139 @@ import {
   CInputGroupText,
   CInputGroup,
   CForm,
-  CCardGroup,
   CCardImage,
   CButton,
+  CRow,
+  CCol,
+  CCardTitle,
+  CCardText,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilPhone, cilText } from '@coreui/icons'
-import ReactImg from 'src/assets/images/avatars/9.jpg'
+import { cilPen, cilPencil, cilPhone, cilRecycle, cilReload, cilText } from '@coreui/icons'
+import ReactImg from 'src/assets/images/avatars/7.jpg'
 
 const User = () => {
   return (
-    <CCardGroup>
-      <CCard className="mb-4">
+    <>
+      <CCard className="col-lg-10 mb-4">
         <CCardHeader>
-          <strong>Пользователь</strong>
+          <strong>Данные пользователя и Фото профиля</strong>
         </CCardHeader>
         <CCardBody>
-          <CForm>
-            <CFormLabel>Имя</CFormLabel>
-            <CInputGroup className="mb-3">
-              <CInputGroupText>
-                <CIcon icon={cilText}></CIcon>
-              </CInputGroupText>
-              <CFormInput type="text" placeholder="Loading..." aria-label="" disabled readOnly />
-            </CInputGroup>
-            <CFormLabel>Фамилия</CFormLabel>
-            <CInputGroup className="mb-3">
-              <CInputGroupText>
-                <CIcon icon={cilText}></CIcon>
-              </CInputGroupText>
-              <CFormInput
-                type="text"
-                placeholder="Loading..."
-                aria-label="Disabled input example"
-                disabled
-                readOnly
+          <CRow>
+            <CCol lg="6" className="mb-4">
+              <CForm>
+                <CFormLabel>Имя</CFormLabel>
+                <CInputGroup className="mb-3">
+                  <CInputGroupText>
+                    <CIcon icon={cilText}></CIcon>
+                  </CInputGroupText>
+                  <CFormInput
+                    type="text"
+                    placeholder="Loading..."
+                    aria-label=""
+                    disabled
+                    readOnly
+                  />
+                </CInputGroup>
+                <CFormLabel>Фамилия</CFormLabel>
+                <CInputGroup className="mb-3">
+                  <CInputGroupText>
+                    <CIcon icon={cilText}></CIcon>
+                  </CInputGroupText>
+                  <CFormInput
+                    type="text"
+                    placeholder="Loading..."
+                    aria-label="Disabled input example"
+                    disabled
+                    readOnly
+                  />
+                </CInputGroup>
+                <CFormLabel>Адрес электронный почты</CFormLabel>
+                <CInputGroup className="mb-3">
+                  <CInputGroupText>@</CInputGroupText>
+                  <CFormInput
+                    type="text"
+                    placeholder="Loading..."
+                    aria-label="Disabled input example"
+                    disabled
+                    readOnly
+                  />
+                </CInputGroup>
+                <CFormLabel>Номер телефона</CFormLabel>
+                <CInputGroup className="mb-3">
+                  <CInputGroupText>
+                    <CIcon icon={cilPhone}></CIcon>
+                  </CInputGroupText>
+                  <CFormInput
+                    type="text"
+                    placeholder="Loading..."
+                    aria-label="Disabled input example"
+                    disabled
+                    readOnly
+                  />
+                </CInputGroup>
+                <div className="d-flex mt-4">
+                  <CButton className="btn-primary">Изменить Данные</CButton>
+                </div>
+              </CForm>
+            </CCol>
+            <CCol className="mb-4 d-flex flex-column align-items-center">
+              <CCardImage
+                src={ReactImg}
+                className="mb-3 p-4"
+                style={{ width: '250px', height: '250px', objectFit: 'cover', borderRadius: '50%' }}
               />
-            </CInputGroup>
-            <CFormLabel>Адрес электронный почты</CFormLabel>
-            <CInputGroup className="mb-3">
-              <CInputGroupText>@</CInputGroupText>
-              <CFormInput
-                type="text"
-                placeholder="Loading..."
-                aria-label="Disabled input example"
-                disabled
-                readOnly
-              />
-            </CInputGroup>
-            <CFormLabel>Номер телефона</CFormLabel>
-            <CInputGroup className="mb-3">
-              <CInputGroupText>
-                <CIcon icon={cilPhone}></CIcon>
-              </CInputGroupText>
-              <CFormInput
-                type="text"
-                placeholder="Loading..."
-                aria-label="Disabled input example"
-                disabled
-                readOnly
-              />
-            </CInputGroup>
+              <div className="d-flex">
+                <CButton className="btn-primary m-1">
+                  <CIcon icon={cilPencil}></CIcon>
+                </CButton>
+                <CButton className="btn-danger m-1 text-white">Удалить</CButton>
+              </div>
+            </CCol>
+          </CRow>
+        </CCardBody>
+      </CCard>
+      <CCard className="col-lg-10 mb-4">
+        <CCardHeader>
+          <strong>Управление аккаунтом</strong>
+        </CCardHeader>
+        <CCardBody>
+          <CForm className="row g-3">
+            <div className="col-auto">
+              <CCardTitle>Если вам нужно выйти из платформы</CCardTitle>
+              <CCardText>
+                <span className="text-warning">
+                  Пожалуйста, убедитесь, что перед выходом из системы выполнены все необходимые
+                  действия. Любые несохраненные данные могут быть утеряны. После выхода вам
+                  потребуется повторно ввести свои учетные данные для доступа к системе.
+                </span>
+              </CCardText>
+            </div>
+            <div className="col-auto">
+              <CButton color="warning" type="submit" className="mb-3 text-white">
+                Выход
+              </CButton>
+            </div>
+            <div className="col-auto">
+              <CCardTitle>Если вам нужно удалить аккаунт</CCardTitle>
+              <CCardText>
+                <span className="text-warning">Будьте внимательны: </span>
+                <span className="text-danger">
+                  Удаление аккаунта является необратимым процессом. После удаления все ваши данные
+                  будут утеряны навсегда и восстановить аккаунт будет невозможно!
+                </span>
+              </CCardText>
+            </div>
+            <div className="col-auto">
+              <CButton color="danger" type="submit" className="mb-3 text-white">
+                Удалить аккаунт
+              </CButton>
+            </div>
           </CForm>
         </CCardBody>
       </CCard>
-      <CCard className="mb-4">
-        <CCardHeader>
-          <strong>Фото профиля</strong>
-        </CCardHeader>
-        <CCardBody className="d-flex flex-column align-items-center">
-          <CCardImage 
-            src={ReactImg} 
-            className='m-1'
-            style={{ width: '250px', height: '250px', objectFit: 'cover', borderRadius: '50%' }} 
-          />
-          <div className="mt-3 ">
-            <CButton className="btn-primary m-1">Изменить</CButton>
-            <CButton className="btn-danger m-1 text-white">Удалить</CButton>
-          </div>
-        </CCardBody>
-      </CCard>
-    </CCardGroup>
+    </>
   )
 }
 
