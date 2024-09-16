b2f2c8dfd75ff36cf2dfa244af12bacea1c552c0
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-09-16 22:32:51 +0500

    changes

--- src/config-global.ts
+++ src/config-global.ts
@@ -10,6 +10,6 @@ export type ConfigValue = {
 // ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
-  appName: 'Minimal UI',
+  appName: 'Road to the Dream',
   appVersion: packageJson.version,
 };
